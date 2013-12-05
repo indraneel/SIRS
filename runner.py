9bfe97fb782a258e93d06819c0e0ae18d27684a6
Stewart Smith <stewartLsmith12@gmail.com>
2013-12-05 03:00:04 +0000

    make professor objects from parsed data

--- runner.py
+++ runner.py
@@ -1,4 +1,24 @@
-import models.py
+import models
 import os
 
+professorDict = {}
+courseDict = {}
 
+def makeObject(dirpath, filename):
+    f = open(os.sep.join([dirpath, filename]), 'r')
+    for line in f:
+        fields = line.split("|")
+        fields[len(fields)-1] = fields[len(fields)-1][:-1]  # getting rid of newline
+        pName = fields[0]
+        if pName not in professorDict:
+            professorDict[pName] = models.Professor(pName)
+
+fileCount = 0
+for (dirpath, dirnames, filenames) in os.walk("./parsed-data/"):
+    for filename in filenames:
+        fileCount += 1
+        if fileCount > 10:
+            break
+        if filename[-4:] == ".txt":
+            makeObject(dirpath, filename)
+print professorDict
