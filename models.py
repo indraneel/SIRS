c1ea5ad46d7202014db52e86ef35b3f153030c51
Stewart Smith <stewartLsmith12@gmail.com>
2013-12-05 03:40:02 +0000

    build professor matrix

--- models.py
+++ models.py
@@ -3,13 +3,22 @@ class Professor:
         self.name = name
         self.all_sections = []
 
-    def add_section(self):
-        pass
+    def add_section(self, newSection):
+        self.all_sections.append(newSection)
+
+    def get_matrix(self):
+        matrix = []
+        for section in self.all_sections:
+            matrix.append(section.ratings)
+        return matrix
+
 
 class Section:
-    def __init__(self, courseID, ratings):
+    def __init__(self, courseID, professor, ratings):
         self.courseID = courseID
+        self.professor = professor
         self.ratings = ratings
+
 
 class Course:
     def __init__(self, courseID):
--- runner.py
+++ runner.py
@@ -1,17 +1,23 @@
-import models
+import models as m
 import os
 
 professorDict = {}
 courseDict = {}
 
+"""parse and make Section and Professor objects"""
 def makeObject(dirpath, filename):
     f = open(os.sep.join([dirpath, filename]), 'r')
     for line in f:
         fields = line.split("|")
         fields[len(fields)-1] = fields[len(fields)-1][:-1]  # getting rid of newline
         pName = fields[0]
+        CID = fields[1]
+        newSection = m.Section(CID, pName, fields[2:12])
+        #Check if professor in dictionary and add it
         if pName not in professorDict:
-            professorDict[pName] = models.Professor(pName)
+            professorDict[pName] = m.Professor(pName)
+        currProfessor = professorDict[pName]
+        currProfessor.add_section(newSection)
 
 fileCount = 0
 for (dirpath, dirnames, filenames) in os.walk("./parsed-data/"):
@@ -21,4 +27,4 @@ for (dirpath, dirnames, filenames) in os.walk("./parsed-data/"):
             break
         if filename[-4:] == ".txt":
             makeObject(dirpath, filename)
-print professorDict
+print professorDict['BRILL GARY'].get_matrix()
